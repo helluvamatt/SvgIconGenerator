f6bff574a24760967ff4cc7c1c077a13565197dc
Matt Schneeberger <matt@leveragesoftware.group>
2025-11-04 23:14:33 -0600

    fix: Nullable error in "generated"/injected attribute

--- src/SvgIconGenerator/Injected/GenerateIconsAttribute.cs
+++ src/SvgIconGenerator/Injected/GenerateIconsAttribute.cs
@@ -19,13 +19,20 @@ namespace SvgIconGenerator
         /// <summary>
         /// Initializes a new instance of the <see cref="GenerateIconsAttribute"/> class.
         /// </summary>
+        public GenerateIconsAttribute()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GenerateIconsAttribute"/> class.
+        /// </summary>
         /// <param name="globPattern">
         /// Optional glob pattern to filter SVG files from AdditionalFiles.
         /// If not specified, all SVG files in AdditionalFiles will be included.
         /// Supports * (wildcard) and ** (recursive) patterns.
-        /// Example: "node_modules/lucide-static/icons/*.svg"
+        /// Example: <code>"node_modules/lucide-static/icons/*.svg"</code>
         /// </param>
-        public GenerateIconsAttribute(string? globPattern = null)
+        public GenerateIconsAttribute(string globPattern)
         {
         }
     }
